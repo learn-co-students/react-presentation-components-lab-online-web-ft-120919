72949cd506cf547da6ed31f88481679532a4f440
github username <meliajm@gmail.com>
2020-04-24 11:28:15 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+changeContent = () => {
+  console.log('really?')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.changeContent} />
   </div>,
   document.getElementById('root')
 );
