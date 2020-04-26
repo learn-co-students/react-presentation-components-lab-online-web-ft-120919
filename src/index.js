832de1005ea62d6a1fd0212d796232b28faf2f87
Adrian082818 <mohabiradrian@gmail.com>
2020-04-26 19:39:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {
+      window.title = "Don't Know"
+    }} />
   </div>,
   document.getElementById('root')
 );
